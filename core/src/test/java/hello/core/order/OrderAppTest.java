42b73dd7f1a1e205ac0a498752e9c6da54d3321d
Lee9Bin <imagen33@daum.net>
2023-08-03 22:16:35 +0900

    다양한 의존관계 주입 방법

--- core/src/test/java/hello/core/order/OrderAppTest.java
+++ core/src/test/java/hello/core/order/OrderAppTest.java
@@ -29,4 +29,12 @@ public class OrderAppTest {
 
         Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
     }
+
+    // @Test
+    // void fieldInjectionTest(){
+    //     OrderServiceImpl orderService1 = new OrderServiceImpl();
+    //     // 먼가 더미데이터를 가지고 테스트를 해보고 싶은데 필드 주입을 하면 넣어줄 방법이 없다.
+    //     // ->그래서 set메서드를 만들어서 해야한다...
+    //     orderService1.createOrder(1l,"item",10000);
+    // }
 }
--- core/src/test/java/hello/core/scan/AutoAppConfigTest.java
+++ core/src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -1,7 +1,9 @@
 package hello.core.scan;
 
 import hello.core.AutoAppConfig;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
+import hello.core.order.OrderServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -12,5 +14,9 @@ public class AutoAppConfigTest {
         AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext(AutoAppConfig.class);
         MemberService memberService = annotationConfigApplicationContext.getBean(MemberService.class);
         Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+
+        OrderServiceImpl orderService = annotationConfigApplicationContext.getBean(OrderServiceImpl.class);
+        MemberRepository memberRepository = orderService.getMemberRepository();
+        System.out.println("memberRepository = " + memberRepository);
     }
 }
