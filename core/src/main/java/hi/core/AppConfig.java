3c11591510e2a979224e5468d122af51605571d0
Lee9Bin <imagen33@daum.net>
2023-10-29 23:44:14 +0900

    새로운 구조와 할인 정책 적용

--- core/src/main/java/hi/core/AppConfig.java
+++ core/src/main/java/hi/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hi.core;
 
+import hi.core.discount.DiscountPolicy;
 import hi.core.discount.FixDiscountPolicy;
+import hi.core.discount.RateDiscountPolicy;
 import hi.core.member.MemberService;
 import hi.core.member.MemberServiceImpl;
 import hi.core.member.MemoryMemberRepository;
@@ -10,10 +12,19 @@ import hi.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        // return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
