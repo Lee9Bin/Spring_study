e773f305eea2a2dc197deb7c00e9a93606b2f923
Lee9Bin <imagen33@daum.net>
2024-05-07 22:28:43 +0900

    HTTP 요청 파라미터 - @ModelAttribute 쿼리 파라미터를 통해 객체 생성 할 작업을 자동화 해준다.

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,9 +1,11 @@
 package hello.springmvc.basic.request;
 
+import hello.springmvc.basic.HelloData;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -105,4 +107,39 @@ public class RequestParamController {
         return "ok";
     }
 
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    //    public String modelAttributeV1(@RequestParam String username, @RequestParam int age) {
+    //        HelloData helloData = new HelloData();
+    //        helloData.setUsername(username);
+    //        helloData.setAge(age);
+    // 위와 같은 코드를 @ModelAttribute 로 단번에 해결!
+    /**
+     * 요청 파라미터의 이름으로 HelloData 객체의 프로퍼티를 찾는다. 그리고 해당 프로퍼티의 setter를 호출해서
+     * 파라미터의 값을 입력(바인딩) 한다.
+     * 예) 파라미터 이름이 username 이면 setUsername() 메서드를 찾아서 호출하면서 값을 입력한다
+     */
+    public String modelAttributeV1(@ModelAttribute HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(),
+                helloData.getAge());
+        return "ok";
+    }
+
+    /**
+     * @ModelAttribute 생략 가능
+     */
+    @RequestMapping("/model-attribute-v2")
+    @ResponseBody
+    public String modelAttributeV2(HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(),
+                helloData.getAge());
+        return "ok";
+    }
+
+    /**
+     * @requestParam 과 @ModelAttribute 둘다 생략이 가능한데 아래와 같은 규칙을 적용하는것을 기억하기
+     * 스프링은 해당 생략시 다음과 같은 규칙을 적용한다.
+     * String , int , Integer 같은 단순 타입 = @RequestParam
+     * 나머지 = @ModelAttribute (argument resolver 로 지정해둔 타입 외)
+     */
 }
