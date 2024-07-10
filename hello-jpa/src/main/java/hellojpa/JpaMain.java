6aac92ec7443e221bea2eaf486ea3b59792a9f34
Lee9Bin <imagen33@daum.net>
2024-07-10 23:05:49 +0900

    요구사항 분석과 기본 매핑 현재는 테이블 설계에 맞춘 방식 -> 테이블의 외래키를 객체 필드에 존재

--- hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -12,13 +12,7 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         //code
-
         try {
-            Member member1 = em.find(Member.class, 1L);
-            Member member2 = em.find(Member.class, 1L);
-
-            System.out.println(member1 == member2);
-
             tx.commit();
         } catch (Exception e){
             tx.rollback();
