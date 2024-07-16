1ca0b91841d2ee370ad73b4f9f170619bd9c663e
Lee9Bin <imagen33@daum.net>
2024-07-16 21:49:32 +0900

    단방향 연관관계

--- hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,5 +1,7 @@
 package hellojpa;
 
+import hellojpa.domain.Member;
+import hellojpa.domain.Team;
 import jakarta.persistence.*;
 
 public class JpaMain {
@@ -11,10 +13,29 @@ public class JpaMain {
 
         EntityTransaction tx = em.getTransaction();
         tx.begin();
-        //code
+        // code
         try {
+            // 팀 저장
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            // 회원 저장
+            Member member = new Member();
+            member.setName("member1");
+            member.setTeam(team); // 단방향 연관관계 설정, 참조 저장
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            // 조회
+            Member findMember = em.find(Member.class, member.getId());
+            // 참조를 사용해서 연관관계 조회
+            Team findTeam = findMember.getTeam();
+
             tx.commit();
-        } catch (Exception e){
+        } catch (Exception e) {
             tx.rollback();
         } finally {
             em.close();
