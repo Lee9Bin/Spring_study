a71a5a1fe2dbef92a74fb84a0ba10221a1518ce6
Lee9Bin <imagen33@daum.net>
2024-08-21 16:12:36 +0900

    Junit5와 assertJ활용

--- cafekiosk/src/test/java/gyub/cafekiosk/unit/CafeKioskTest.java
+++ cafekiosk/src/test/java/gyub/cafekiosk/unit/CafeKioskTest.java
@@ -1,6 +1,6 @@
 package gyub.cafekiosk.unit;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +9,7 @@ import gyub.cafekiosk.unit.beverages.Americano;
 class CafeKioskTest {
 
 	@Test
-	void add() {
+	void add_menual_test() {
 		//given
 		CafeKiosk cafeKiosk = new CafeKiosk();
 		//when
@@ -17,5 +17,28 @@ class CafeKioskTest {
 		//then
 		System.out.println(cafeKiosk.getBeverages().size());
 		System.out.println(cafeKiosk.getBeverages().get(0).getName());
+	}
+
+	@Test
+	void add() {
+		CafeKiosk cafeKiosk = new CafeKiosk();
+		//when
+		cafeKiosk.add(new Americano());
+
+		assertThat(cafeKiosk.getBeverages().size()).isEqualTo(1);
+		assertThat(cafeKiosk.getBeverages().get(0).getName()).isEqualTo("아메리카노");
+	}
+
+	@Test
+	void remove() {
+		CafeKiosk cafeKiosk = new CafeKiosk();
+		Americano americano = new Americano();
+		//when
+		cafeKiosk.add(americano);
+
+		assertThat(cafeKiosk.getBeverages()).hasSize(1);
+
+		cafeKiosk.remove(americano);
+		assertThat(cafeKiosk.getBeverages()).isEmpty();
 	}
 }
